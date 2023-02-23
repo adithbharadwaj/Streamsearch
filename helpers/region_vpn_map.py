2465c9770c5462a7d5f1ea79f795ea0c2850b795
karthikmurali60 <karthik.murali60@gmail.com>
2023-02-23 14:04:21 -0800

    Minor fixes

--- helpers/region_vpn_map.py
+++ helpers/region_vpn_map.py
@@ -1,8 +1,11 @@
 REGION_VPN_MAP = {
     'US': [["ExpressVPN", "https://expressvpn.com"], ["NordVPN", "https://nordvpn.com"]],
-    'UK': [["ExpressVPN", "https://expressvpn.com"], ["NordVPN", "https://nordvpn.com"]],
+    'GB': [["ExpressVPN", "https://expressvpn.com"], ["NordVPN", "https://nordvpn.com"]],
     'IN': [["ExpressVPN", "https://expressvpn.com"], ["NordVPN", "https://nordvpn.com"]]
 }
 
 def region_vpn_map(locale_code):
-    return REGION_VPN_MAP[locale_code]
+    if locale_code not in REGION_VPN_MAP:
+        return []
+    else:
+        return REGION_VPN_MAP[locale_code]
