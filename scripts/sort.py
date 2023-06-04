d4071eda8debc62ba2c50ef0ba489e57b3abcb9b
karthikmurali60 <karthik.murali60@gmail.com>
2023-06-04 16:30:14 -0700

    Fix sort script

--- scripts/sort.py
+++ scripts/sort.py
@@ -1,10 +1,10 @@
 import csv
 
-with open('media_dump/movies.csv', 'r') as csvfile:
+with open('../dataset/movies.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
-    sorted_rows = sorted(reader, key=lambda row: row[4], reverse=True)  # Sort by Age column
+    sorted_rows = sorted(reader, key=lambda row: row[4], reverse=True)  # Sort by Popularity column
 
-with open('media_dump/sorted_movies.csv', 'w', newline='') as csvfile:
+with open('../dataset/sorted_movies.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     for row in sorted_rows:
         writer.writerow(row)
